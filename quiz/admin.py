f172a90474551ad65e7c6fa67ecd01405a2fe126
RyanLin11 <ryanlin118@gmail.com>
2020-12-24 16:45:50 -0500

    Choice model now tabular in-line with Question in admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from .models import Question
-from .models import Exam
-from .models import Choice
-from .models import UserExam
-from .models import UserAnswer
+from .models import Question, Exam, Choice, UserExam, UserAnswer
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    max_num=4
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
 
 # Register your models here.
 admin.site.register(Exam)
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(UserExam)
 admin.site.register(UserAnswer)
