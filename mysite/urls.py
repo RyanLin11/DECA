b868786febb572982b5a6842c450959e0f6115f8
RyanLin11 <ryanlin118@gmail.com>
2021-01-14 12:10:40 -0500

    Added link to admin

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/admin/', admin.site.urls),
     path('exam/', include('quiz.urls')),
     path('', include('accounts.urls')),
     path('leaderboard/', include('leaderboard.urls')),
